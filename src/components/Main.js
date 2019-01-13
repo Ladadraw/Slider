0666acf8a67cf1901e0b4320f38aa6ab0ecacead
Ladadraw <ladadraw@gmail.com>
2019-01-13 18:55:08 +0300

    Третий комит

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,8 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
 
+import Slider from './Slider';
+
 const Repair = styled.div`
     color: #fff;
     font-weight: 700;
@@ -51,7 +53,7 @@ class Main extends Component{
                     <CallBtn>Отправить заявку!</CallBtn>
                 </Col>
                 <Col lg={6} lgOffset={1}>
-                    
+                    <Slider />
                 </Col>
             </Row>
         );
